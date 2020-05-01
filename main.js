b87fa8c02224efd1352f7c0221549829c416933c
hajime hatakeyama <63620706+hajime-hatakeyama@users.noreply.github.com>
2020-05-01 19:17:20 +0900

    milli

--- main.js
+++ main.js
@@ -14,8 +14,8 @@
     const d = new Date(Date.now() - startTime + elapsedTime);
     const m = String(d.getMinutes()).padStart(2,'0');
     const s = String(d.getSeconds()).padStart(2,'0');
-    const ms = String(d.getMilliseconds()).padStart(3,'');
-    timer.textContent = `${m}:${s}.${ms}`;
+    const ms = String(d.getMilliseconds()).padStart(3,'0');
+    timer.textContent = `${m}:${s}:${ms}`;
 
     timeoutId = setTimeout(() => {
       countUp();
